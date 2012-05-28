8d522f0f0076167ea1339fdb2e895d226251483d
andrefreitas <p.andrefreitas@gmail.com>
2012-05-28 23:16:22 +0100

    Added code comments.

--- code/include/color.h
+++ code/include/color.h
@@ -1,4 +1,10 @@
- #ifdef __APPLE__
+ /****************************************************************************
+ * Author:      - André Freitas, p.andrefreitas@gmail.com
+ * Author:      - Tiago Tavares, tiagomctavares@gmail.com
+ * Author:      - Olivier Alves, olivieralves1@gmail.com
+ * Copyright:   - 29/05/2012, CAL, FEUP
+ ****************************************************************************/
+#ifdef __APPLE__
     #define RESET_COLOR "\e[1;30m"
     #define GREEN "\e[1;32m"
     #define RED "\e[1;31m"
@@ -20,13 +26,21 @@
 
 #include <iostream>
 
-// A class with static methods for changing the
+/** Color - A class for defining the consolo color
+ * This class use static methods for changing the console output color.
+ */
 
 class Color {
 public:
-	//Changes the current color of text
+	/** Changes the current color of text
+	 * @param color the color (use constants above)
+	 */
 	static void color(std::string color);
 
-	//Color to change, string to print, line=true for new line
+	/** Changes a color of a given string
+	 * @param color the color (use constants above)
+	 * @param print the string to be in the output
+	 * @param line true if is a line (\n)
+	 */
 	static void color(std::string color, std::string print, bool line);
 };
